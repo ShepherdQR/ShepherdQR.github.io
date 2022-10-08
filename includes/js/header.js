68695bc4b50139868dea5e54f6d47aa1c89e1ce9
ShepherdQR <37508446+ShepherdQR@users.noreply.github.com>
2022-10-08 23:09:16 +0800

    book0048

--- includes/js/header.js
+++ includes/js/header.js
@@ -3,17 +3,18 @@
  * @Date: 2022-10-06 21:41:23
  * @Github: https://github.com/ShepherdQR
  * @LastEditors: Shepherd Qirong
- * @LastEditTime: 2022-10-08 19:57:58
+ * @LastEditTime: 2022-10-08 20:05:37
  * Copyright (c) 2019--20xx Shepherd Qirong. All rights reserved.
  */
 
-
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-45586344-1', 'shepherd.org');
-ga('send', 'pageview');
+function styleHeader(){
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-45586344-1', 'shepherd.org');
+    ga('send', 'pageview');
+}
 
 MathJax = {
     tex: {inlineMath: [['$', '$'], ['\\(', '\\)']]}
